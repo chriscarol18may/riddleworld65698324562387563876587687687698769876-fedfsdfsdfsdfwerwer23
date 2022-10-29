02352d60b973c721ec420243ffb00fa28a2c74de
chriscarol18may <78288189+chriscarol18may@users.noreply.github.com>
2022-10-29 20:30:45 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,43 @@
+onEvent("button1", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button2", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button3", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("label2", "click", function( ) {
+  open("https://www.youtube.com/channel/UCP7v6r5wlAQqi4Rn2OMypxQ");
+});
+onEvent("button5", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button4", "click", function( ) {
+  setScreen("screen4");
+});
+onEvent("button7", "click", function( ) {
+  setScreen("screen5");
+});
+onEvent("label3", "click", function( ) {
+  open("https://docs.google.com/forms/d/115idj5nm_q7YIhmRmZnikLNlol9JbRl5kcsSZ-nn90k/edit");
+});
+onEvent("button8", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button9", "click", function() {
+  setScreen("screen6");
+});
+onEvent("button9", "click", function( ) {
+  playSound("assets/yt5s.com-iPhone-6s-Remix-ringtone-__-Remix-beat-Ringtone-.mp3", true);
+});
+onEvent("image10", "click", function( ) {
+  stopSound("assets/yt5s.com-iPhone-6s-Remix-ringtone-__-Remix-beat-Ringtone-.mp3");
+  playSound("assets/call.mp3", true);
+});
+onEvent("image13", "click", function( ) {
+  stopSound("assets/call.mp3");
+});
+onEvent("button9", "click", function( ) {
+  setScreen("screen2");
+});
